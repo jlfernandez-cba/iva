fbb326cdd4b592e779962707b3f4eea06532ddf5
Alexis Martínez Chacón <alemarcha26@gmail.com>
2017-12-01 09:12:18 +0100

    Update host to opencga-1.3.0-rc

--- src/conf/config.js
+++ src/conf/config.js
@@ -21,7 +21,7 @@ const cellbase = {
 };
 
 const opencga = {
-    host: "iva-enod.clinbioinfosspa.es:8080/opencga-1.3.0-dev",
+    host: "iva-enod.clinbioinfosspa.es:8080/opencga-1.3.0-rc",
     version: "v1",
     // asUser: "researchcga", // user@project:study
     projects: [
