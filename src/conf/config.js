f82c71cb2fffbce86d57c073e12ce6fd6cf1b3c1
Alexis Martínez Chacón <alexismartinezchacon@gmail.com>
2018-02-15 09:38:18 +0100

    v1.1.3

--- src/conf/config.js
+++ src/conf/config.js
@@ -56,7 +56,7 @@ const ebiWS = {
 
 const application = {
     title: "MMP-SAS",
-    version: "v1.1.2",
+    version: "v1.1.3",
     logo: "img/cbra_logo_initial.png",
     notifyEventMessage: "notifymessage",
     mmpExtensionWS: {
