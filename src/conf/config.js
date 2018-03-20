a308f766128e872756b13252bf3bd1a0157ae188
Alexis Martínez Chacón <alexismartinezchacon@gmail.com>
2018-03-20 12:55:58 +0100

    set host for iva-ext. v.1.0.0

--- src/conf/config.js
+++ src/conf/config.js
@@ -21,7 +21,7 @@ const cellbase = {
 };
 
 const opencga = {
-    host: "iva-acci.clinbioinfosspa.es:8080/opencga-1.3.2",
+    host: "iva-ext.clinbioinfosspa.es:8080/opencga-1.3.2",
     version: "v1",
     // asUser: "researchcga", // user@project:study
     projects: [
